713564c83718c6730ce2d7f0e19865366e13ab99
davidc8103 <davidc8103@gmail.com>
2025-10-28 14:58:16 -0400

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,11 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+});
+
 app.MapControllers();
 
 DbInitializer.InitDb(app);
