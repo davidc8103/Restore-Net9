498f86e98a62ed560cede84ec2cac8e2f899fa90
davidc8103 <davidc8103@gmail.com>
2025-11-05 23:49:33 -0500

    End of section 8

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,14 +1,41 @@
+import { Grid, Typography } from "@mui/material";
 import ProductList from "./ProductList";
-import { useFetchProductsQuery } from "./catalogApi";
+import { useFetchFiltersQuery, useFetchProductsQuery } from "./catalogApi";
+import Filters from "./Filters";
+import { useAppDispatch, useAppSelector } from "../../app/store/store";
+import AppPagination from "../../app/shared/components/AppPagination";
+import { setPageNumber } from "./catalogSlice";
 
 export default function Catalog() {
-  const {data, isLoading} = useFetchProductsQuery();
-  
-  if (isLoading || !data) return <div>Loading...</div>
+  const productParams = useAppSelector(state => state.catalog);
+  const { data, isLoading } = useFetchProductsQuery(productParams);
+  const {data: filtersData, isLoading: filtersLoading} = useFetchFiltersQuery();
+  const dispatch = useAppDispatch();
+
+  if (isLoading || !data || filtersLoading || !filtersData) return <div>Loading...</div>
 
   return (
-    <div>
-      <ProductList products={data} />
-    </div>
+    <Grid container spacing={4}>
+      <Grid size={3}>
+        <Filters filtersData={filtersData} />
+      </Grid>
+      <Grid size={9}>
+        {data.items && data.items.length > 0 ? (
+          <>
+            <ProductList products={data.items} />
+            <AppPagination
+              metadata={data.pagination}
+              onPageChange={(page: number) => {
+                dispatch(setPageNumber(page))
+                window.scrollTo({top: 0, behavior: 'smooth'})
+              }}
+            />
+          </>
+        ) : (
+          <Typography variant="h5">There are no results for this filter</Typography>
+        )}
+
+      </Grid>
+    </Grid>
   )
 }
