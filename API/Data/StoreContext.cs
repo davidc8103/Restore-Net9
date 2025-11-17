35b326870a4274e521541992acc9e427112a5bf0
davidc8103 <davidc8103@gmail.com>
2025-11-17 10:47:25 -0500

    End of section 9

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,12 +1,25 @@
 using System;
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class StoreContext(DbContextOptions options) : DbContext(options)
+    public class StoreContext(DbContextOptions options) : IdentityDbContext<User>(options)
     {
         public required DbSet<Product> Products { get; set; }
         public required DbSet<Basket> Baskets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>()
+                .HasData(
+                    new IdentityRole { Id = "5b1c64c5-bb1f-4ecd-ad84-1ee7ef496f9a", Name = "Member", NormalizedName = "MEMBER" },
+                    new IdentityRole { Id = "a05beb93-17e6-4ec3-ba73-554a1234ce25", Name = "Admin", NormalizedName = "ADMIN" }
+                );
+        }
     }
 }
